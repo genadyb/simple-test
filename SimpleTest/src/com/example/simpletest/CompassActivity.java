5416e62ed0f5f89c6c460c34d6bfd59d7303ce18
Genady Buchatsky <genady.buchatsky@gmail.com>
2013-04-16 19:05:09 +0300

    test

--- SimpleTest/src/com/example/simpletest/CompassActivity.java
+++ SimpleTest/src/com/example/simpletest/CompassActivity.java
@@ -14,6 +14,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+
 public class CompassActivity extends SimpleTestActivity implements SensorEventListener {
 	
 	public static float m_fRot = 0;
